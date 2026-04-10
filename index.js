1e829f8cb6671255cd21006b6ed47a5dd9ab7cb0
Axados1111 <axadosstudios@outlook.com>
2026-04-10 23:54:18 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const { Client, GatewayIntentBits, Collection } = require('discord.js');
 const fs = require('fs');
-const config = require('./config.json');
 
 const client = new Client({
   intents: [
@@ -14,29 +13,23 @@ client.commands = new Collection();
 
 // Load commands
 const commandFiles = fs.readdirSync('./commands').filter(f => f.endsWith('.js'));
+
 for (const file of commandFiles) {
   const command = require(`./commands/${file}`);
   client.commands.set(command.data.name, command);
 }
 
-// Ready event
-client.once('ready', () => {
-  console.log(`Logged in as ${client.user.tag}`);
-});
+// Load events
+const eventFiles = fs.readdirSync('./events').filter(f => f.endsWith('.js'));
 
-// Interaction handler
-client.on('interactionCreate', async interaction => {
-  if (!interaction.isChatInputCommand()) return;
+for (const file of eventFiles) {
+  const event = require(`./events/${file}`);
 
-  const command = client.commands.get(interaction.commandName);
-  if (!command) return;
-
-  try {
-    await command.execute(interaction);
-  } catch (err) {
-    console.error(err);
-    await interaction.reply({ content: 'Error executing command', ephemeral: true });
+  if (event.once) {
+    client.once(event.name, (...args) => event.execute(...args, client));
+  } else {
+    client.on(event.name, (...args) => event.execute(...args, client));
   }
-});
+}
 
-client.login(config.token);
+client.login(process.env.TOKEN);
